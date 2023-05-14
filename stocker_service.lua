9717d4f650a82ec77c3cb714b40722b9b2c0f636
Justin Hu <justin.hu@alumni.ubc.ca>
2023-05-13 17:17:30 -0700

    fix stocker transfer cap

--- stocker_service.lua
+++ stocker_service.lua
@@ -13,13 +13,25 @@ while true do
   local _, _, _, replyChannel, message, _ = os.pullEvent("modem_message")
   if message.type == "sell" then
     local chest = peripheral.wrap(message.chest) or error("Bad chest to sell from")
-    local pulled = storage.pullItem(peripheral.getName(chest), message.name, message.quantity)
+    local leftToMove = message.quantity
+    local lastMove = 128
+    while leftToMove > 0 and lastMove ~= 0 do
+      lastMove = storage.pullItem(peripheral.getName(chest), message.name, leftToMove)
+      leftToMove = leftToMove - lastMove
+    end
+    local pulled = message.quantity - leftToMove
 
     modem.transmit(replyChannel, channels.STOCKER_CHANNEL, pulled)
     print("Customer sold " .. pulled .. " " .. message.name)
   elseif message.type == "buy" then
     local chest = peripheral.wrap(message.chest) or error("Bad chest to sell from")
-    local pushed = storage.pushItem(peripheral.getName(chest), message.name, message.quantity)
+    local leftToMove = message.quantity
+    local lastMove = 128
+    while leftToMove > 0 and lastMove ~= 0 do
+      lastMove = storage.pushItem(peripheral.getName(chest), message.name, message.quantity)
+      leftToMove = leftToMove - lastMove
+    end
+    local pushed = message.quantity - leftToMove
 
     modem.transmit(replyChannel, channels.STOCKER_CHANNEL, pushed)
     print("Customer bought " .. pushed .. " out of " .. message.quantity .. " " .. message.name)
