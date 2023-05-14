e8b949b344ad029f5da0a4a688b494735fcf0b39
Justin Hu <justin.hu@alumni.ubc.ca>
2023-05-13 23:35:30 -0700

    implement replicater service

--- replicater_service.lua
+++ replicater_service.lua
@@ -7,7 +7,8 @@ modem.open(channels.REPLICATER_CHANNEL)
 
 local stockTank = peripheral.wrap("modern_industrialization:quantum_tank_0") or error("No stock tank attached", 0)
 local useTank = peripheral.wrap("modern_industrialization:quantum_tank_1") or error("No use tank attached", 0)
-local replicator = peripheral.wrap("modern_industrialization:replicator_1") or error("No replicator attached", 0)
+local turtleStorage = peripheral.wrap("modern_industrialization:configurable_chest_13") or
+    error("No temp storage attached", 0)
 local outputBarrel = peripheral.wrap("modern_industrialization:quantum_barrel_1") or
     error("No output barrel attached", 0)
 
@@ -18,20 +19,30 @@ while true do
 
   local chest = peripheral.wrap(message.chest) or error("Bad chest to get template from", 0)
 
-  -- TODO: invoke turtle insert
+  local transferred = turtleStorage.pullItem(peripheral.getName(chest), nil, 1) == 0
+  if transferred ~= 0 then
+    modem.transmit(channels.REPLICATER_TURTLE_CHANNEL, 0, "insert")
 
-  stockTank.pushFluid(peripheral.getName(useTank), nil, 100 * message.quantity)
+    stockTank.pushFluid(peripheral.getName(useTank), nil, 100 * message.quantity)
 
-  while outputBarrel.items()[1] == nil or outputBarrel.items()[1].count < message.quantity do
-    os.sleep(1)
+    while outputBarrel.items()[1] == nil or outputBarrel.items()[1].count < message.quantity do
+      os.sleep(1)
+    end
+
+    while outputBarrel.items()[1] ~= nil do
+      outputBarrel.pushItem(peripheral.getName(chest), nil, nil)
+    end
+
+    modem.transmit(channels.REPLICATER_TURTLE_CHANNEL, 0, "extract")
+
+    while turtleStorage.pushItem(peripheral.getName(chest), nil, nil) == 0 do
+      -- intentionally empty
+    end
+
+    modem.transmit(replyChannel, channels.REPLICATER_CHANNEL, message.quantity)
+    print("Replicated " .. message.quantity .. " items")
+  else
+    modem.transmit(replyChannel, channels.REPLICATER_CHANNEL, 0)
+    print("No item to replicate")
   end
-
-  while outputBarrel.items()[1] ~= nil do
-    outputBarrel.pushItem(peripheral.getName(chest), nil, nil)
-  end
-
-  -- TODO: invoke turtle extract
-
-  modem.transmit(replyChannel, channels.REPLICATER_CHANNEL, message.quantity)
-  print("Replicated " .. message.quantity .. " items")
 end
---
+++ replicater_turtle_service.lua
@@ -0,0 +1,31 @@
+-- replicater service - turtle component
+
+local channels = require("channels")
+
+local modem = peripheral.wrap("top") or error("No modem attached", 0)
+modem.open(channels.REPLICATER_TURTLE_CHANNEL)
+
+local storage = peripheral.wrap("modern_industrialization:configurable_chest_13") or
+    error("No temp storage attached", 0) -- TODO: what's this?
+
+print("Replicater turtle started")
+
+while true do
+  local _, _, _, replyChannel, message, _ = os.pullEvent("modem_message")
+  if message == "insert" then
+    storage.pushItem("front", nil, 1)
+  elseif message == "extract" then
+    turtle.dig()
+    if turtle.getItemDetail(1).name == "modern_industrialization:replicator" then
+      turtle.select(1)
+      turtle.place()
+      turtle.select(2)
+      turtle.dropDown()
+    else
+      turtle.select(2)
+      turtle.place()
+      turtle.select(1)
+      turtle.dropDown()
+    end
+  end
+end
